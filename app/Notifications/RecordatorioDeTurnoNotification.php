33e18f8f5a47ce0729c462156926cb33e935c338
milagrosb02 <arimibor@hotmail.com>
2023-07-29 21:27:05 -0300

    Arregle la hora y fecha de la  notificacion de los turnos

--- app/Notifications/RecordatorioDeTurnoNotification.php
+++ app/Notifications/RecordatorioDeTurnoNotification.php
@@ -7,6 +7,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Carbon\Carbon;
 
 class RecordatorioDeTurnoNotification extends Notification
 {
@@ -42,13 +43,18 @@ class RecordatorioDeTurnoNotification extends Notification
      */
     public function toMail($notifiable)
     {
+         // Formatear las fechas y horas usando Carbon
+        $fecha = Carbon::parse($this->turno->fecha)->format('d/m/Y');
+        $hora = Carbon::parse($this->turno->hora)->format('H:i');
+
         return (new MailMessage)
-                    ->subject('Recordatorio de turno.')
-                    ->line('Recordatorio de turno.')
-                    ->line('Usted esta recibiendo este email como recordatorio de su turno.')
-                    ->line('con la Profesional ' . $this->turno->user->first_name . ' ' . $this->turno->user->last_name)
-                    ->line('el dia de ' . $this->turno->fecha . ' a las ' . $this->turno->hora)
-                    ->line('¡Gracias por su confianza!');
+        ->subject('Recordatorio de turno.')
+        ->line('Recordatorio de turno.')
+        ->line('Usted está recibiendo este email como recordatorio de su turno.')
+        ->line('con la Profesional ' . $this->turno->user->first_name . ' ' . $this->turno->user->last_name)
+        ->line('el día de ' . $fecha . ' a las ' . $hora)
+        ->line('¡Gracias por su confianza!');
+
     }
 
     /**
